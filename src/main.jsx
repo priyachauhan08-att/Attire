05350a2fdac499ba1760ee0abd1f326281140aab
priyagithub19 <priya.chauhan117317@marwadiuniversity.ac.in>
2026-09-11 20:10:18 +0530

    Switch to HashRouter to fix reload 404s on GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename="/Attire">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
